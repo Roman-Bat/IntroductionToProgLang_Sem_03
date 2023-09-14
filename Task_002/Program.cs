0c6165f31154c136453f67c84bf319b48b7fdfe0
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 10:44:19 +0300

    Выполнил Task 002

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая по заданному номеру четверти, показывает
+диапазон возможных координат точек в этой четверти (x и y). */
+
+// Дилог с пользователем и получение номера четверти
+int TalkUser (string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+// Проверка на существование введённой четверти
+bool CheckRange (int number)
+{
+    if (number < 1 || number > 4) return false;
+    else return true;
+}
+
+string RangeNumber (int quarter, string num)
+{
+    if (quarter == 1 && num == "x") return "+"; 
+    else if (quarter == 1 && num == "y") return "+";
+    else if (quarter == 2 && num == "x") return "-";
+    else if (quarter == 2 && num == "y") return "+";
+    else if (quarter == 3 && num == "x") return "-";
+    else if (quarter == 3 && num == "y") return "-";
+    else if (quarter == 4 && num == "x") return "+";
+    else if (quarter == 4 && num == "y") return "-";
+    else return "0";
+}
+
+int quarter = TalkUser("Введите четверть: ");
+if (CheckRange(quarter) == true)
+{
+    Console.Write("Диапазон чисел Х = " + RangeNumber(quarter, "x") + " бесконечность, до " + " Y = " +  RangeNumber(quarter, "y") + " бесконечность");
+}
+else Console.Write("Нет такой четверти");
+
