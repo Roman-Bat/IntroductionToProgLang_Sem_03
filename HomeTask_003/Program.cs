48bcfc91c74140e79277225ce0a7afc3bd426b01
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 15:58:32 +0300

    Решил домашнюю задачу №3

--- HomeTask_003/Program.cs
+++ HomeTask_003/Program.cs
@@ -4,3 +4,34 @@
 3 -> 1, 4, 9.
 5 -> 1, 8, 27, 64, 125
 */
+using System;
+
+public class Answer
+{
+   static void ShowCube(int N)
+    {
+      // Введите свое решение ниже
+      int cube = 0;
+     for (int i = 1; i <= N; i ++)
+     {
+        cube = (i*i*i);
+        Console.WriteLine(cube);
+     }
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 2;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}
