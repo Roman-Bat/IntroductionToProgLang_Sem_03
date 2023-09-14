9dae0214d8d2adb556d2cce4106a38a8672dfde7
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 16:00:55 +0300

    Решил задачу №4

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -5,3 +5,34 @@
 2 -> 1,4
 */
 
+using System;
+
+public class Answer
+{
+   static void ShowCube(int N)
+    {
+      // Введите свое решение ниже
+      int qadro = 0;
+     for (int i = 1; i <= N; i ++)
+     {
+        qadro = (i*i);
+        Console.WriteLine(qadro);
+     }
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 6;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}
